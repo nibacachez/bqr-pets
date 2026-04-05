b72d23fe8f1421810b2ad06c257b031d211e0bda
Nicolas <n.ibacache.z@gmail.com>
2026-04-05 03:00:55 -0400

    fix: next.config.ts sin conflictos

--- bqr/next.config.ts
+++ bqr/next.config.ts
@@ -1,19 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-<<<<<<< HEAD
-  turbopack: {
-    root: __dirname,
-  },
   images: {
     unoptimized: true,
   },
 };
 
 export default nextConfig;
-=======
-  /* config options here */
-};
-
-export default nextConfig;
->>>>>>> 40f82eab594e550b16bb444d4a922e8d85565fae
